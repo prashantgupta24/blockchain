2aeac34f8a6bf8272507ee0d4340bb4b5d0851cc
Prashant Gupta <prashantgupta@us.ibm.com>
2018-06-19 17:29:10 -0700

    minor changes

--- public.sh
+++ public.sh
@@ -1,4 +1,10 @@
-curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip --output ngrok.zip
-unzip ngrok.zip
-rm ngrok.zip
+if [ -e ngrok ]
+then
+    echo "ngrok already installed. Running ..."
+else
+  curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip --output ngrok.zip
+  unzip ngrok.zip
+  rm ngrok.zip
+fi
+
 ./ngrok http 8001
